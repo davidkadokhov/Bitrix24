03f4be11218e69daf1dba6c1d0074461dec93d0f
davidkadokhov <kadoh87@gmail.com>
2019-09-20 18:31:33 -0400

    test

--- src/test/java/com/cybertek/pages/ActivityPage.java
+++ src/test/java/com/cybertek/pages/ActivityPage.java
@@ -5,4 +5,7 @@ import com.cybertek.utilities.BasePage;
 public class ActivityPage extends BasePage {
 
 
+    public static void main(String[] args) {
+        System.out.println("this is for Git");
+    }
 }
