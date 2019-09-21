385b3cad1cb79bbef230a061ffead7c733097a8a
davidkadokhov <kadoh87@gmail.com>
2019-09-20 20:15:10 -0400

    branch merging

--- src/test/java/com/cybertek/pages/ActivityPage.java
+++ src/test/java/com/cybertek/pages/ActivityPage.java
@@ -7,5 +7,7 @@ public class ActivityPage extends BasePage {
 
     public static void main(String[] args) {
         System.out.println("this is for Git");
+
+        System.out.println("trying mergeing to master");
     }
 }
